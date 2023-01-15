0913a25a2936904e519a590c47eb7a7a5ff0b549
perfumelim <perfumelim@gmail.com>
2023-01-15 22:24:31 +0900

    graphQL Code Generator 에러 디버깅

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { ColorModeSwitcher } from './components/ColorModeSwitcher';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { ChakraProvider, Box, Text, theme } from '@chakra-ui/react';
+import FilmList from './components/film/FilmList';
 
 const apolloClient = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
@@ -12,7 +13,7 @@ export const App = () => (
   <ApolloProvider client={apolloClient}>
     <ChakraProvider theme={theme}>
       <Box>
-        <Text>Ghibli GraphQL</Text>
+        <FilmList />
       </Box>
     </ChakraProvider>
   </ApolloProvider>
