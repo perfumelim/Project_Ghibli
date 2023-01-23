f57fe4c98f296048f301ab5650eca96ffbe108aa
perfumelim <perfumelim@gmail.com>
2023-01-23 22:10:44 +0900

    typeorm 버전에 따른 에러 디버깅

--- server/src/db/db-client.ts
+++ server/src/db/db-client.ts
@@ -1,15 +1,16 @@
-import User from "..//entities/User";
-import { Connection, createConnection } from "typeorm";
+import { DataSource } from 'typeorm';
+import User from '..//entities/User';
 
-export const createDB = async (): Promise<Connection> => 
-createConnection({
-type: 'mysql',
-host: 'localhost',
-port: 3306,
-database: 'ghibli_graphql',
-username: 'root',
-password: 'qwer1234',
-logging: !(process.env.NODE_ENV === 'production'),
-synchronize: true,
-entities: [User]
-})
+export const createDB = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  database: 'ghibli_graphql',
+  username: 'root',
+  password: 'qwer1234',
+  logging: !(process.env.NODE_ENV === 'production'),
+  synchronize: true,
+  entities: [User],
+});
+
+createDB.connect();
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,21 @@
-import "reflect-metadata";
-import express from "express";
-import { ApolloServer} from "apollo-server-express";
-import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
-import http from "http";
-import { FilmResolver } from "./resolvers/Film";
-import { buildSchema } from "type-graphql";
-import { CutResolver } from "./resolvers/Cut";
-import {createDB} from './db/db-client'
-import { UserResolver } from "./resolvers/User";
+import 'reflect-metadata';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import http from 'http';
+import { FilmResolver } from './resolvers/Film';
+import { buildSchema } from 'type-graphql';
+import { CutResolver } from './resolvers/Cut';
+import { createDB } from './db/db-client';
+import { UserResolver } from './resolvers/User';
 
 async function main() {
-  await createDB();
   const app = express();
 
   const apolloServer = new ApolloServer({
-  schema: await buildSchema({
-    resolvers: [FilmResolver, CutResolver, UserResolver]
-  }),
+    schema: await buildSchema({
+      resolvers: [FilmResolver, CutResolver, UserResolver],
+    }),
     plugins: [ApolloServerPluginLandingPageLocalDefault()],
   });
   await apolloServer.start();
@@ -25,7 +24,7 @@ async function main() {
   const httpServer = http.createServer(app);
 
   httpServer.listen(process.env.PORT || 4000, () => {
-    if (process.env.NODE_ENV !== "production") {
+    if (process.env.NODE_ENV !== 'production') {
       console.log(`
           server started on => http://localhost:4000
           graphql playground => http://localhost:4000/graphql`);
