c8857634f97e9e731862cd02967e65821a9da404
perfumelim <perfumelim@gmail.com>
2023-01-27 18:09:05 +0900

    login Mutation 함수 구성

--- server/src/resolvers/User.ts
+++ server/src/resolvers/User.ts
@@ -1,6 +1,6 @@
 import argon2  from "argon2";
 import { IsEmail, IsString } from "class-validator";
-import { Arg, Field, InputType, Mutation, Resolver } from "type-graphql";
+import { Arg, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
 import User from "../entities/User";
 
 @InputType()
@@ -11,6 +11,30 @@ export class SignUpInput {
 
   @Field() @IsString() password: string;
 
+}
+
+@InputType({description: '로그인 인풋 데이터'})
+export class LoginInput {
+  @Field() @IsString() emailOrUsername: string;
+  @Field() @IsString() password: string;
+}
+
+@ObjectType({description: '필드 에러 타입'})
+class FieldError {
+  @Field() field: string;
+  @Field() message: string;
+}
+
+@ObjectType({description: '로그인 반환 데이터'})
+class LoginResponse {
+  @Field(()=> [FieldError], {nullable: true})
+  errors?: FieldError[];
+
+  @Field(()=> User, {nullable: true})
+  user?: User;
+
+  @Field({nullable: true})
+  accessToken?: string;
 }
 
 @Resolver(User)
@@ -29,4 +53,24 @@ export class UserResolver {
     await User.insert(newUser)
     return newUser
   }
+
+  @Mutation(()=> LoginResponse)
+  public async login(
+    @Arg('loginInput') loginInput: LoginInput,): Promise<LoginResponse> {
+      const {emailOrUsername, password} = loginInput;
+
+      const user = await User.findOne({where: [{email: emailOrUsername}, {username: emailOrUsername}]});
+      if(!user) return {
+        errors: [{field: 'emailOrUsername', message: '해당하는 유저가 없습니다'}]
+      };
+
+      const isValid = await argon2.verify(user.password, password);
+      if(!isValid)
+      return {
+        errors: [
+          {field: 'password', message: '비밀번호를 올바르게 입력해주세요.'}
+        ]
+      };
+      return {user};
+    } 
 }
